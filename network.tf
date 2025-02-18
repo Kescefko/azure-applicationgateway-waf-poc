f797d2d79adba695ee37d2845f818575ebbadd0c
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-18 20:23:15 +0100

    add vm, network, rg and related variables

---
+++ network.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet" {
+  name = var.vnet_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "subnet_appgw" {
+  name = var.subnet_appgw_name
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet_web" {
+  name = var.subnet_web_name
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.2.0/24"]
+}
---
+++ ubuntu.tf
@@ -0,0 +1,52 @@
+resource "azurerm_network_interface" "vm_nic" {
+  name = "vm-nic"
+  location = azurerm_resource_group.r
+  resource_group_name = azurerm_resource_group.poc.name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.subnet_web.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "backend_vm" {
+  name = "backend-vm"
+  resource_group_name = azurerm_resource_group.poc.name
+  location = azurerm_resource_group.poc.location
+  size = "Standard_B1s"
+
+  admin_username = var.vm_admin_username
+  admin_password = var.vm_admin_password
+  disable_password_authentication = false
+
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "18.04-LTS"
+    version = "latest"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = var.vm_admin_username
+      password = var.vm_admin_password
+      host = azurerm_network_interface.vm_nic.private_ip_address
+    }
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install nginx -y",
+      "echo 'Hello from POC Web App' | sudo tee /var/www/html/index.html",
+      "sudo systemctl restart nginx"
+    ]
+  }
+}
+
