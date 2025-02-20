d68ce181dfea7651367bda0ba6bb869510b52f68
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-02-20 21:05:46 +0100

    add waf policy

--- appgw_waf.tf
+++ appgw_waf.tf
@@ -61,7 +61,7 @@ resource "azurerm_application_gateway" "appgw" {
   }
 
   waf_configuration {
-    enabled = false
+    enabled = true
     firewall_mode = "Prevention"
     rule_set_type = "OWASP"
     rule_set_version = "3.2"
---
+++ waf_policy.tf
@@ -0,0 +1,28 @@
+resource "azurerm_application_gateway_waf_policy" "example_waf_policy" {
+  name                = "example-waf-policy"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+
+  custom_rules {
+    name     = "block_sql_injection"
+    priority = 1
+    rule_type = "MatchRule"
+    action   = "Block"
+    match_conditions {
+      match_variable = "RequestHeader"
+      operator       = "Contains"
+      match_values   = ["' OR 1=1 --", "DROP TABLE"]
+    }
+  }
+
+  rule_set {
+    rule_set_type    = "OWASP"
+    rule_set_version = "3.2"
+  }
+
+  managed_rules {
+    enabled = true
+    rule_set_type = "OWASP"
+    rule_set_version = "3.2"
+  }
+}
